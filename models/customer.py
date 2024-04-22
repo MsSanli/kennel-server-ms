3bd8c267e1c9d8aea61d9494636486da7ddd4a62
Maria Sanli <marisanli.93@gmail.com>
2024-04-21 23:39:24 -0400

    transient changes

---
+++ models/customer.py
@@ -0,0 +1,5 @@
+class Customer():
+    """creating class by looking at properties that are hard-coded"""
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
---
+++ models/location.py
@@ -0,0 +1,5 @@
+class Location():
+    """creating class by looking at properties that are hard-coded"""
+    def __init__(self, id, address):
+        self.id = id
+        self.address = address
